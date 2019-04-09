c28c9aab5618484adb35176d0871455c45080ab5
nereasolabarrieta <nerea.solabarrieta@opendeusto.es>
2019-04-09 08:25:31 +0200

    Segundo do while clsMenu

--- clsMenu.c
+++ clsMenu.c
@@ -6,11 +6,14 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main(void) {
-	int opcion;
+	int opcion, opcion2;
 
 	do {
-		printf("1. Introducir balance");
+		printf(" 1. Introducir balance");
 		printf("\n 2. Gestionar compras");
 		printf("\n 3. Salir");
 		printf(" \n\n Introduzca una opción del 1-3:");
@@ -18,8 +21,43 @@ int main(void) {
 		scanf("%d", &opcion);
 		switch (opcion) {
 		case 1:
+//			introducirBalance();
 			break;
 		case 2:
+
+//			if (existsFile("Balance.txt") == 0) {
+//				introducirBalance();
+//			} else {
+
+
+				do {
+					printf(" 1. Introducir cliente");
+					printf("\n 2. Introducir proveedor");
+					printf("\n 3. Contabilizar venta");
+					printf("\n 4. Contabilizar compra");
+					printf("\n 5. Otros gastos");
+					printf("\n 6. Visualizar estado contable");
+					printf("\n 7. Salir");
+					printf(" \n\n Introduzca una opción del 1-7:");
+
+				scanf("%d", &opcion2);
+				switch (opcion2) {
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+				case 6:
+					break;
+				}
+			} while (opcion2 != 7);
+//			}
+
 			break;
 		}
 	} while (opcion != 3);
